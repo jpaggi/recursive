60b62c4ab4d14d45232217ffd6c2a2899ad01ba4
Joe Paggi <jpaggi@mit.edu>
2016-02-27 22:50:00 -0500

    comparison to graveley rs introns

---
+++ sjr/compare_to_graveley.py
@@ -0,0 +1,28 @@
+import sys
+
+graveley_file = open('../../data/recursiveintrons.bed')
+
+graveley = []
+for line in graveley_file:
+	chrom, start, end, igv, rs, strand = line.strip().split('\t')
+	rs = map(int, rs.split(','))
+	start, end = int(start), int(end)
+	graveley += [(chrom, start, end, strand, rs)]
+
+
+for line in sys.stdin:
+	chrom, start, end, sample, entropy, strand, seq1, seq2 = line.strip().split('\t')
+	start, end = int(start), int(end)
+	overlap = False
+	seen = False
+	for (g_chrom, g_start, g_end, g_strand, g_rs) in graveley:
+		# - strand indexing the same
+		# + strand g_rs == end + 1
+		if g_chrom == chrom and g_strand == strand and (g_start <= start <= g_end or g_start <= end <= g_end):
+			if strand == '+':
+				seen = seen or (end + 1 in g_rs)
+			else:
+				seen = seen or (start in g_rs)
+			overlap = True
+	if not overlap or not seen:
+		print chrom, start, end, entropy, seq1, seq2, strand
